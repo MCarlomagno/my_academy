296262188d2cd19242ad9f7f7de15f97da5af912
mcarlomagno <marcoscarlomagno2@gmail.com>
2020-07-09 17:21:48 -0300

    add: login

--- lib/env/enviroment.dart
+++ lib/env/enviroment.dart
@@ -1,8 +1,4 @@
 class Enviroment {
-
   static String _apiUrl = 'https://salty-river-66710.herokuapp.com';
   static String get apiUrl => _apiUrl;
 } 
-
-// URL sample
-// https://www.quandl.com/api/v3/datasets/WGEN/GUY_SL_EMP_UNDR_FE_ZS?start_date=2017-12-31&end_date=2017-12-31&api_key=bfyTDxRheky4onfAYSH_
